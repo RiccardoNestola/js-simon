12b237d27eabb5d32d74b2c92d366de255a42410
RiccardoNestola <riccardinux@gmail.com>
2022-12-02 17:48:31 +0100

    complete

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 
-let button = document.getElementById("start")
+const buttonElement = document.getElementById('start');
 
 let score = 0;
+let stop = false;
 
-button.addEventListener("click", function() {
+buttonElement.addEventListener('click', function(){
+    if(!stop){
+        if (score < 10){
+            score++;
 
-    if (score < 10){
-        score++;
-        buttonElement.innerHTML = "#" + score;        
+            buttonElement.innerHTML = "Start" + " " + score; 
+
+        } else{
+            stop=true;
+            setTimeout(() => {
+                stop=false;
+                score=0;
+                buttonElement.innerHTML = "Start" + " " + score; 
+            },5000);
+        }
     }
-    
 });
